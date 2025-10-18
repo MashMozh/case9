6577db22e9342094f6dfb17cb0a4c3a70ff1059f
MashMozh <m.mozhaitseva@g.nsu.ru>
2025-10-18 19:55:31 +0700

    Create color.py

---
+++ color.py
@@ -0,0 +1,23 @@
+print('Доступные цвета: красный, оранжевый, желтый, '
+      'зеленый, синий, фиолетовый, розовый, черный, серый, белый')
+
+
+def get_color_choice(color):
+    available_colors = ['красный', 'оранжевый', 'желтый', 'зеленый',
+                        'синий', 'фиолетовый', 'розовый', 'черный',
+                        'серый', 'белый']
+    while color not in available_colors:
+        color = input('Ошибка ввода цвета. Пожалуйста, повторите попытку: ')
+    return color
+
+
+color_first = input('Выберите первый цвет из предложенных выше: ')
+color_first = get_color_choice(color_first)
+
+color_second = input('Выберите второй цвет из предложенных выше: ')
+color_second = get_color_choice(color_second)
+
+
+
+
+
